c8353b3414c6a18f7ed95ebcd6014e4cc3826539
Fabricio-Barreto <63824002+Fabricio-Barreto@users.noreply.github.com>
2022-09-13 22:38:07 -0300

    splash and home screen

---
+++ mobile/src/components/Background/styles.ts
@@ -0,0 +1,9 @@
+import { StyleSheet } from 'react-native';
+import { THEME } from '../../theme';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1, // para indicar a tela toda.
+    backgroundColor: THEME.COLORS.BACKGROUND_800
+  }
+});
