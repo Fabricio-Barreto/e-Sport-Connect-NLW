64f8762df6f30c7d759d11bfb6c43872670ea75c
Fabricio-Barreto <63824002+Fabricio-Barreto@users.noreply.github.com>
2022-09-17 16:36:26 -0300

    Mobile Game Screen

--- web/src/components/Form/Input.tsx
+++ web/src/components/Form/Input.tsx
@@ -3,8 +3,6 @@ import { InputHTMLAttributes } from 'react'
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {}
 
 export function Input(props: InputProps) {
-
-
     return (
         <input 
         {...props}
